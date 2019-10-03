91551163a87c4d86c4b98473a398c2a09d7f8c61
Ruchika06 <ruchikaasharan@gmail.com>
2019-10-03 21:11:24 +0530

    Schema and keys added

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const PORT= process.env.PORT || 5000;
+const mongoose = require('mongoose');
+
+//DB CONFIG
+
+const db = require('./config/keys').MongoURI;
+mongoose.connect(db, {useNewUrlParser: true})
+.then(()=> console.log('MongoDB COnnected....'))
+.catch(err=> console.log(err));
 
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+//Bodyparser
+app.use(express.urlencoded({extended: false}));
 
 //Routes
 
